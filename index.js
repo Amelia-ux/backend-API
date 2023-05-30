6ac3d7445046bf198313aa596de6281fa770b587
amelia <meliamel.9977@gmail.com>
2023-05-30 13:40:39 +0700

    new

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ async function updateFirestoreDocument(documentRef, result) {
 }
 
 // Menjalankan server pada port tertentu
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server berjalan pada http://localhost:${port}`);
 });
