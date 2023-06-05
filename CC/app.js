7aa068038560f757c553e2d0d79203e7f7c2bec1
amelia <meliamel.9977@gmail.com>
2023-06-05 13:27:35 +0700

    manage folders

---
+++ CC/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = require("./routes/routes");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Load routes
+app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  console.log("Response success")
+  res.send("Response Success!")
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
